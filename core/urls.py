4603585a6770ce399c1755bde9fc5d3b594e091b
mertlinux <77293848+mertcaliskanlnx@users.noreply.github.com>
2021-07-29 14:52:14 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('mertadmin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('',include('base.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
